a0c183f7444bc8f72c0d431971334f0ff5c58e1e
Ben-Lee-2019 <liben_go@163.com>
2024-06-19 22:58:35 +0800

    动态规划练习

---
+++ notion_2_git.py
@@ -0,0 +1,93 @@
+import os
+import shutil
+import pandas as pd
+
+
+def rename_notion(notion_path):
+    for filename in os.listdir(notion_path):
+        full_path = os.path.join(notion_path, filename)
+
+        if filename.endswith(".md"):
+            new_filename = filename[:-36] + ".md"
+        elif filename.endswith(".csv"):
+            new_filename = filename[:-37] + ".csv"
+        else:
+            new_filename = filename[:-33]
+        new_full_path = os.path.join(notion_path, new_filename)
+        os.rename(full_path, new_full_path)
+
+
+def rename_problem(problem_path):
+    # 获取所有md文档的奇怪后缀，保存到map中，用于替换md中的图片路径
+    dic = {}
+
+    # 这里将奇怪后缀去掉
+    for filename in os.listdir(problem_path):
+        full_path = os.path.join(problem_path, filename)
+
+        if filename.endswith(".md"):
+            new_filename = filename[:-36] + ".md"
+            replace_content = filename[-35:-3]
+            dic[new_filename] = '%20' + replace_content
+        else:
+            new_filename = filename[:-33]
+        new_full_path = os.path.join(problem_path, new_filename)
+        os.rename(full_path, new_full_path)
+
+    # 读取文件夹中的markdown格式文档，如果文档中有数据包含集合A中的32位字符串，将其去掉
+    for filename in os.listdir(problem_path):
+        if filename.endswith(".md"):  # 仅处理markdown格式文档
+            file_path = os.path.join(problem_path, filename)
+            with open(file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+                content = content.replace(dic[filename], '')
+                with open(file_path, 'w', encoding='utf-8') as file:
+                    file.write(content)
+
+
+def move_tag(path, csv_path, git_path):
+    ## 将csv中的数据解析成  question:tag的形式
+
+    df = pd.read_csv(csv_path, encoding='utf-8')
+    data_dict = df.set_index('Problems')['Tag'].to_dict()
+
+    for key, value in data_dict.items():
+        # question 带了一个. 需要去掉
+        key = key.replace('.', '')
+
+        source_path = os.path.join(path, key)
+        target_path = os.path.join(git_path, value)
+
+        ## 目标文件夹不存在就创建
+        if not os.path.exists(target_path):
+            os.makedirs(target_path)
+
+        ## 不是所有的都有题解
+        if os.path.exists(source_path):
+            shutil.copytree(source_path, os.path.join(target_path, key), dirs_exist_ok=True)
+
+        source_path = source_path + ".md"
+        if os.path.exists(source_path):
+            shutil.copy(source_path, target_path)
+
+
+if __name__ == '__main__':
+
+    ## notion 的第一层路径
+    parent_path = "Leetcode review "
+    ## problem 的路径
+    problem_path = parent_path + "/Problem(总）"
+    ## 题目csv文件的路径
+    problem_csv = parent_path + "/Problem(总）.csv"
+    ## git文件夹的路径
+    git_folder_path = ""
+
+
+    ## 将最上层文件重命名
+    rename_notion(parent_path)
+    ## 重命名题目
+    rename_problem(problem_path)
+    # 将题解md文档移动到对应的git文件夹中
+    move_tag(problem_path, problem_csv, git_folder_path)
+
+    shutil.copy(os.path.join(parent_path, "踩坑注意点.md"), git_folder_path)
